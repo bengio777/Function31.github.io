1a6871fa4ab56e52c37450b8cfcc10d9d5eb12bd
jaimielogan <jaimielogan@gmail.com>
2016-08-11 16:29:08 -0600

    Side Bar Added

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  }
+);
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
